ff4ef93eb88da73c5b134a79ade4ece97524903e
nwpuzhengyan <35477241+nwpuzhengyan@users.noreply.github.com>
2020-02-28 16:45:59 +0800

    modified code
    not only retain the alignment of max AS, add a merge_cigar string function

--- expand_cigar.py
+++ expand_cigar.py
@@ -2,7 +2,7 @@ import argparse
 from PIL import Image
 import numpy as np
 
-def expand_cigar(cigar):
+def expand_cigar(cigar, reverse):
     cigar_str=''
     i=0
     while i<len(cigar):
@@ -13,8 +13,21 @@ def expand_cigar(cigar):
         if (cigar[i]!='S' and cigar[i]!='H' and cigar[i]!='I'):
             cigar_str=cigar_str+cigar[i]*int(num)
         i+=1
-    return cigar_str
+    if reverse=='False':
+        return cigar_str
+    else:
+        return cigar_str.lower()
 
+def merge_cigar(cigar1, cigar2, pos1, pos2):
+    if pos1<pos2:
+        n=pos2-pos1-len(cigar1)
+        cigar_str=cigar1+'0'*n+cigar2
+        res=[pos1,cigar_str]
+    else:
+        n = pos1 - pos2 - len(cigar2)
+        cigar_str = cigar2 + '0' * n + cigar1
+        res = [pos2, cigar_str]
+    return res
 
 parser = argparse.ArgumentParser()
 parser.add_argument("TXT", help='cigar file')
@@ -27,12 +40,13 @@ read_cigar={}
 while line:
     line = line.split()
     if line[0] in read_cigar:
-        if (int(line[3])>int(read_cigar[line[0]][2])):
-            read_cigar[line[0]]=[line[1], expand_cigar(line[2]),line[3]]
+        pos1=int(read_cigar[line[0]][0])
+        cigar1 = read_cigar[line[0]][1]
+        pos2 = int(line[1])
+        cigar2 = expand_cigar(line[2],line[4])
+        read_cigar[line[0]]=merge_cigar(cigar1,cigar2,pos1,pos2)
     else:
-        read_cigar[line[0]] = [line[1], expand_cigar(line[2]), line[3]]
-
-
+        read_cigar[line[0]] = [line[1], expand_cigar(line[2],line[4])]
     line = f.readline()
 
 f.close()
@@ -43,7 +57,7 @@ i=0
 for key in read_cigar:
     s=int(read_cigar[key][0])
     e=int(read_cigar[key][0])+len(read_cigar[key][1])
-    WIN_S=100038214
+    WIN_S=106206161
     s=max(0,s-WIN_S)
     e=min(20000,e-WIN_S)
     for pos in range(0,e-s):
@@ -51,10 +65,14 @@ for key in read_cigar:
             clipping=WIN_S-int(read_cigar[key][0])
         else:
             clipping=0
-        if read_cigar[key][1][clipping+pos]=='D':
+        if read_cigar[key][1][clipping+pos]=='D' or read_cigar[key][1][clipping+pos]=='d':
             matrix[i][s+pos] = 2
-        else:
+        elif read_cigar[key][1][clipping+pos].isupper():
             matrix[i][s + pos] = 1
+        elif read_cigar[key][1][clipping+pos]=='0':
+            matrix[i][s + pos] = 0
+        else:
+            matrix[i][s + pos] = 3
     i+=1
 
 c = Image.new("RGB", (len(read_cigar),2000))
@@ -68,9 +86,10 @@ for i in range (0,len(read_cigar)):
            c.putpixel([i,j-image_s],(0,0,0))
        elif matrix[i,j]==2:
            c.putpixel([i, j - image_s], (255, 0, 0))
+       elif matrix[i,j]==3:
+           c.putpixel([i, j - image_s], (0,200,255))
        else:
            c.putpixel([i, j-image_s], (255, 255, 0))
 
 c.show()
-c.save("large_sample.png")
-
+c.save("Nor_sample.png")
